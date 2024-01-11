c45a02224edbbee154d55ed6973b40bd24aab3c5
securemist <gongjiatian00@qq.com>
2024-01-11 14:02:06 +0800

    开发websocket

---
+++ mist-chat-server/src/main/java/cc/xmist/MistChatServerApplication.java
@@ -0,0 +1,13 @@
+package cc.xmist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MistChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MistChatServerApplication.class, args);
+    }
+
+}
---
+++ mist-chat-server/src/test/java/cc/xmist/mistchatserver/MistChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.xmist.mistchatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MistChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
